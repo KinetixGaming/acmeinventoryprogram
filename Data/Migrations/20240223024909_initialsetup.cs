a9bf7060aee39ceeaacb24b58019d7159c7bf749
KinetixGaming <lawr3572@gmail.com>
2024-03-03 21:26:42 -0500

    Initial Commit

---
+++ Data/Migrations/20240223024909_initialsetup.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AcmeDistributing.Data.Migrations
+{
+    public partial class initialsetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Order",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    OrderedBy = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Quantity = table.Column<int>(type: "int", nullable: false),
+                    PackagingType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CaseSize = table.Column<int>(type: "int", nullable: false),
+                    SKU = table.Column<int>(type: "int", nullable: false),
+                    OrderConfirmation = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Order", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Order");
+        }
+    }
+}
